d9142afe8cca176cf602bdcced80cef1d8a7323c
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-07-06 19:34:25 +0200

    Setup routes

--- routes/new-messages.js
+++ routes/new-messages.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET New Message. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('new');
 });
 
 module.exports = router;
